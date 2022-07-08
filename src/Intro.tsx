a46e09485ecee52da6814618825f83575415dd24
Ondřej Hájek <ondra.hajku@seznam.cz>
2022-07-08 17:16:08 +0200

    Patch[1.2]
    Section 0: layouts addons

--- src/Intro.tsx
+++ src/Intro.tsx
@@ -1,14 +1,14 @@
 export const Intro = ({size}:{size:{width: number, height: number}}) => {
   return (
     <div className="w-full h-auto bg-black font-roboto flex text-white md:flex-row flex-col min-h-full">
-      <div className="h-auto flex md:text-8xl text-5xl md:m-0 my-10 mx-auto">
+      <div className="h-auto flex md:text-8xl text-5xl md:m-0 mt-8 mx-auto">
         <div className="grid p-2 my-auto">
           <p>o</p>
           <p>n</p>
           <p>d</p>
           <p>ř</p>
           <p>e</p>
-          <p className="row-span-2" style={size.width>768?{ fontSize: "192px", lineHeight: "150px", letterSpacing: "-20px" }:{fontSize: "150px", lineHeight: "50px", letterSpacing: "-25px"}}>
+          <p className="row-span-2" style={size.width>767?{ fontSize: "192px", lineHeight: "150px", letterSpacing: "-20px" }:{fontSize: "150px", lineHeight: "50px", letterSpacing: "-25px", marginLeft: "-20px"}}>
             j
           </p>
 
@@ -19,10 +19,10 @@ export const Intro = ({size}:{size:{width: number, height: number}}) => {
         </div>
       </div>
 
-      <div className="flex-1 flex m-10 md:m-0">
+      <div className="flex-1 flex mb-10 mt-4 md:m-0">
         <div className="m-auto">
-          <h1 className="text-center mb-4 font-bold text-3xl">my new logo</h1>
-          <div className="w-60 h-60 border-4 border-white rounded-full">
+          <h1 className="text-center mb-4 font-bold text-3xl md:text-4xl">my new logo</h1>
+          <div className="w-60 h-60 border-8 border-white">
 
           </div>
         </div>
