dd1c288909193cab0286f377f8f162558aee2854
Ondřej Hájek <ondra.hajku@seznam.cz>
2022-07-30 18:50:19 +0200

    Patch[4.8]
    Section 1: re-work

--- src/components/Second.tsx
+++ src/components/Second.tsx
@@ -4,30 +4,26 @@ import "./components.scss";
 export default function Second({ count, height, block, width }: { count: number; height: number; block: boolean; width: number }) {
   return (
     <div className="flex" style={{ height: height, width: width }}>
-      {block ? (
-        <div className="title w-fit">Skills</div>
-      ) : (
-        <div className="w-full h-full flex flex-col justify-between">
-          {skills.map((item, index) => (
-            <div className={`flex w-fit ${index % 2 === 0 ? "skill-even" : "skill-odd"}`} key={item} style={{ animation: `${index % 2 === 0 ? "move-even 2s" : "move-odd 2s"}` }}>
-              
-              {index % 2 === 0 && (
-                <div className="md:w-32 w-16 md:h-32 h-16 my-auto">
-                  <img src={require(`../images/${item}.png`)} alt="" className="md:w-32 w-16 md:h-32 h-16 my-auto" />
-                </div>
-              )}
-              
-              <p>{item}</p>
-
-              {index % 2 === 1 && (
-                <div className="md:w-32 w-16 md:h-32 h-16 my-auto">
-                  <img src={require(`../images/${item}.png`)} alt="" className="md:w-32 w-16 md:h-32 h-16 my-auto" />
-                </div>
-              )}
+      <div className="w-full h-full flex justify-center flex-col xl:flex-row">
+        <div className="flex m-6">
+          <div className="rounded-full circle grid grid-rows-3 grid-cols-2 relative overflow-hidden m-auto w-fit">
+            <div className="w-full h-full z-20" style={{ background: skills[0].color, clipPath: "polygon(0 0, 100% 0, 100% 100%, 50% 100%, 0 70%)" }}></div>
+            <div className="w-full h-full z-20" style={{ background: skills[1].color, clipPath: "polygon(0 0, 100% 0, 100% 70%, 50% 100%, 0 100%)" }}></div>
+            <div className="w-full h-full relative z-10">
+              <div className="absolute w-full" style={{ background: skills[2].color, clipPath: "polygon(0 0, 50% 18%, 50% 81%, 0% 100%)", height: "160%", top: "-30%" }}></div>
             </div>
-          ))}
+            <div className="w-full h-full relative z-10 rotate-180">
+              <div className="absolute w-full" style={{ background: skills[3].color, clipPath: "polygon(0 0, 50% 18%, 50% 82%, 0% 100%)", height: "160%", top: "-30%" }}></div>
+            </div>
+
+            <div className="w-full h-full -z-20" style={{ background: skills[4].color, clipPath: "polygon(0 0, 100% 0, 100% 100%, 50% 100%, 0 70%)" }}></div>
+            <div className="w-full h-full -z-20" style={{ background: skills[5].color, clipPath: "polygon(0 0, 100% 0, 100% 70%, 50% 100%, 0 100%)" }}></div>
+            <div className="absolute top-1/2 left-1/2 mid z-50"></div>
+          </div>
         </div>
-      )}
+
+        <div className=" flex-1" style={{ minWidth: 300 }}></div>
+      </div>
     </div>
   );
 }
--- src/support.tsx
+++ src/support.tsx
@@ -1,1 +1,8 @@
-export const skills = ["react", "typescript", "firebase", "tailwind", "scss", "cypress"]
+export const skills = [
+  { tech: "react", color: "#61DBFB", code: "" },
+  { tech: "typescript", color: "#007acc", code: "" },
+  { tech: "firebase", color: "#F5820D", code: "" },
+  { tech: "tailwind", color: "#41a9b4", code: "" },
+  { tech: "scss", color: "#cc6699", code: "" },
+  { tech: "cypress", color: "#2ce39a", code: "" },
+];
