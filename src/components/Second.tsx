9b8b7fffb63d0cf02dad0ac8052f755ad2b63081
Ondřej Hájek <ondra.hajku@seznam.cz>
2022-07-31 10:42:09 +0200

    Patch[4.8.1]
    You spin me right round...

--- src/components/Second.tsx
+++ src/components/Second.tsx
@@ -1,28 +1,47 @@
-import React from "react";
-import { skills } from "../support";
+import React, { useState } from "react";
+import { imgs, skills } from "../support";
 import "./components.scss";
 export default function Second({ count, height, block, width }: { count: number; height: number; block: boolean; width: number }) {
+  const [rotate, setRotate] = useState(0);
+  console.log(rotate)
   return (
     <div className="flex" style={{ height: height, width: width }}>
-      <div className="w-full h-full flex justify-center flex-col xl:flex-row">
-        <div className="flex m-6">
-          <div className="rounded-full circle grid grid-rows-3 grid-cols-2 relative overflow-hidden m-auto w-fit">
-            <div className="w-full h-full z-20" style={{ background: skills[0].color, clipPath: "polygon(0 0, 100% 0, 100% 100%, 50% 100%, 0 70%)" }}></div>
-            <div className="w-full h-full z-20" style={{ background: skills[1].color, clipPath: "polygon(0 0, 100% 0, 100% 70%, 50% 100%, 0 100%)" }}></div>
-            <div className="w-full h-full relative z-10">
-              <div className="absolute w-full" style={{ background: skills[2].color, clipPath: "polygon(0 0, 50% 18%, 50% 81%, 0% 100%)", height: "160%", top: "-30%" }}></div>
-            </div>
-            <div className="w-full h-full relative z-10 rotate-180">
-              <div className="absolute w-full" style={{ background: skills[3].color, clipPath: "polygon(0 0, 50% 18%, 50% 82%, 0% 100%)", height: "160%", top: "-30%" }}></div>
+      <div className="w-full h-full flex justify-center flex-col md:flex-row ">
+        <div className="flex m-6 transition-all" style={{ transform: `rotate(${rotate*60}deg)` }}>
+          <div className="rounded-full circle grid grid-rows-3 grid-cols-2 relative  m-auto w-fit overflow-hidden">
+
+            <div onClick={()=>setRotate(1)}>
+              <div className="w-full z-20 row-start-1 col-start-1" style={{ height: "150%", background: skills[0].color, clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)" }}></div>
             </div>
 
-            <div className="w-full h-full -z-20" style={{ background: skills[4].color, clipPath: "polygon(0 0, 100% 0, 100% 100%, 50% 100%, 0 70%)" }}></div>
-            <div className="w-full h-full -z-20" style={{ background: skills[5].color, clipPath: "polygon(0 0, 100% 0, 100% 70%, 50% 100%, 0 100%)" }}></div>
+            <div onClick={()=>setRotate(0)}>
+              <div className="w-full h-full z-20 row-start-1 col-start-2" style={{ height: "150%", background: skills[1].color, clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)" }}></div>
+            </div>
+
+            <div className="relative z-40" onClick={()=>setRotate(2)}>
+              <div className="w-full absolute" style={{background: skills[2].color, height: "166%", top: "-33%", clipPath: "polygon(0% 100%, 0 0, 100% 50%)" }}></div>
+            </div>
+            <div className="relative z-40" onClick={()=>setRotate(5)}>
+              <div className="w-full absolute" style={{ background: skills[3].color, height: "166%", top: "-33%", clipPath: "polygon(100% 100%, 100% 0%, 0% 50%)" }}></div>
+            </div>
+
+            <div onClick={()=>setRotate(3)}>
+              <div className="w-full h-full -z-20 row-start-3 col-start-1 relative" style={{ bottom: "50%", height: "150%", background: skills[4].color, clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)" }}></div>
+            </div>
+
+            <div onClick={()=>setRotate(4)}>
+              <div className="w-full h-full -z-20 row-start-3 col-start-2 relative" style={{ bottom: "50%", height: "150%", background: skills[5].color, clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)" }}></div>
+            </div>
+
             <div className="absolute top-1/2 left-1/2 mid z-50"></div>
           </div>
         </div>
 
-        <div className=" flex-1" style={{ minWidth: 300 }}></div>
+        <div className="flex-1 p-3" style={{ minWidth: 300 }}>
+          <div className="border-slate-500 border-8 w-full h-full rounded-3xl relative">
+            <img src={require(`../images/${imgs[rotate]}.png`)} alt="" className="md:w-44 md:h-44 w-20 h-20 absolute" />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/support.tsx
+++ src/support.tsx
@@ -1,8 +1,9 @@
 export const skills = [
-  { tech: "react", color: "#61DBFB", code: "" },
-  { tech: "typescript", color: "#007acc", code: "" },
-  { tech: "firebase", color: "#F5820D", code: "" },
-  { tech: "tailwind", color: "#41a9b4", code: "" },
-  { tech: "scss", color: "#cc6699", code: "" },
-  { tech: "cypress", color: "#2ce39a", code: "" },
+  { tech: "cypress", color: "#2ce39a", code: "", rotate: 1},
+  { tech: "react", color: "#61DBFB", code: "" , rotate: 0},
+  { tech: "firebase", color: "#F5820D", code: "", rotate: 2},
+  { tech: "typescript", color: "#007acc", code: "", rotate: 5 },
+  { tech: "scss", color: "#cc6699", code: "", rotate: 3 },
+  { tech: "tailwind", color: "#41a9b4", code: "", rotate: 4 },
 ];
+export const imgs = ["react", "cypress", "firebase", "scss", "tailwind", "typescript"]
